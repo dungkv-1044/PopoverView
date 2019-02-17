e6b01229046da864c5f7ff588540033ed7b43900
Dung <dung@Dungs-iMac.local>
2019-02-17 16:09:34 +0700

    init popover view

--- ExamplePopoverView/ViewController.swift
+++ ExamplePopoverView/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnHidePopover: UIButton!
+    @IBOutlet var popoverView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.popoverView.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func showDetail(_ sender: Any) {
+        self.popoverView.center = self.view.center
+        self.btnHidePopover.isHidden = false
+        UIView.transition(with: self.view,
+                          duration: 0.2,
+                          options: [UIView.AnimationOptions.transitionCrossDissolve],
+                          animations: {
+                            self.view.addSubview(self.popoverView)
+                           
+        },
+                          completion: { (_) in
+                            
+        })
+        
+    }
+    
+    @IBAction func doDone(_ sender: Any) {
+        self.btnHidePopover.isHidden = true
+        self.popoverView.removeFromSuperview()
+    }
+    
 }
 
